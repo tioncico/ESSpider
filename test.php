4d213d42863a04ccbbb0d06beb4196a09c43b6bf
tioncico <1067197739@qq.com>
2019-06-23 18:18:14 +0800

    commit

--- test.php
+++ test.php
@@ -1,16 +1,52 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Tioncico
- * Date: 2019/6/13 0013
- * Time: 9:35
+ * User: tioncico
+ * Date: 19-6-16
+ * Time: 下午5:35
  */
-
 include "./vendor/autoload.php";
 \EasySwoole\EasySwoole\Core::getInstance()->initialize();
-go(function () {
-    $domain = 'www.php20.cn';
-    $httpClient = new \EasySwoole\HttpClient\HttpClient("http://apidata.chinaz.com/CallAPI/Whois?key=93b828b408db4f6db8972c80d0934cf4&domainName=www.php20.cn");
-    $response = $httpClient->postRequest($data);
-    var_dump($response->getBody());
+go(function (){
+    \App\Spider\RedisLogic::clearConsumeMap();//清除消费map
+    \App\Spider\RedisLogic::clearProduceMap();//清除生产map
+    \App\Spider\RedisLogic::clearConsumeList();//清除消费队列
+    \App\Spider\RedisLogic::clearProduceList();//清除生产队列
+    //新增默认队列
+    \App\Spider\RedisLogic::addProduce('http://moe.005.tv/cosplay/');
 });
+//go(function (){
+//    $init = new \AutomaticGeneration\Init();
+//    $init->initBaseModel();
+//    $init->initBaseController();
+//    $db = \EasySwoole\MysqliPool\Mysql::defer('mysql');
+//
+//    $mysqlTable = new \AutomaticGeneration\MysqlTable($db, \EasySwoole\EasySwoole\Config::getInstance()->getConf('MYSQL.database'));
+//    $tableName = 'cosplay_img_list';
+//    $tableColumns = $mysqlTable->getColumnList($tableName);
+//    $tableComment = $mysqlTable->getComment($tableName);
+//    $path = 'img';
+//
+//    $beanConfig = new \AutomaticGeneration\Config\BeanConfig();
+//    $beanConfig->setBaseNamespace("App\\Model\\".$path);
+////    $beanConfig->setBaseDirectory(EASYSWOOLE_ROOT . '/' .\AutomaticGeneration\AppLogic::getAppPath() . 'Model');
+//    $beanConfig->setTablePre('');
+//    $beanConfig->setTableName('cosplay_img_list');
+//    $beanConfig->setTableComment($tableComment);
+//    $beanConfig->setTableColumns($tableColumns);
+//    $beanBuilder = new \AutomaticGeneration\BeanBuilder($beanConfig);
+//    $result = $beanBuilder->generateBean();
+//    $path = 'img';
+//    $modelConfig = new \AutomaticGeneration\Config\ModelConfig();
+//    $modelConfig->setBaseNamespace("App\\Model\\".$path);
+////    $modelConfig->setBaseDirectory(EASYSWOOLE_ROOT . '/' .\AutomaticGeneration\AppLogic::getAppPath() . 'Model');
+//    $modelConfig->setTablePre("");
+//    $modelConfig->setExtendClass(\App\Model\BaseModel::class);
+//    $modelConfig->setTableName("cosplay_img_list");
+//    $modelConfig->setTableComment($tableComment);
+//    $modelConfig->setTableColumns($tableColumns);
+//    $modelBuilder = new \AutomaticGeneration\ModelBuilder($modelConfig);
+//    $result = $modelBuilder->generateModel();
+//    var_dump($result);
+//
+//});
