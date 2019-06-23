c1a207abe3ef6aeddbff28decf28d8b0e1bf1ce4
tioncico <1067197739@qq.com>
2019-06-23 18:49:41 +0800

    commit

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -10,6 +10,9 @@ namespace EasySwoole\EasySwoole;
 
 
 use App\Process\Consume;
+use App\Process\DomainHandel;
+use App\Process\ParsePage;
+use App\Process\ProcessInit;
 use App\Process\Produce;
 use EasySwoole\EasySwoole\Swoole\EventRegister;
 use EasySwoole\EasySwoole\AbstractInterface\Event;
@@ -27,18 +30,25 @@ class EasySwooleEvent implements Event
 
         $redisConfig = new \EasySwoole\RedisPool\Config(Config::getInstance()->getConf('REDIS'));
         $a = Redis::getInstance()->register('redis', $redisConfig);
-        $a->setMaxObjectNum(50);
-//        $mysqlConfig = new \EasySwoole\Mysqli\Config(Config::getInstance()->getConf('MYSQL'));
-//        \EasySwoole\MysqliPool\Mysql::getInstance()->register('mysql', $mysqlConfig);
+        $a->setMaxObjectNum(100);
+        $mysqlConfig = new \EasySwoole\Mysqli\Config(Config::getInstance()->getConf('MYSQL'));
+        \EasySwoole\MysqliPool\Mysql::getInstance()->register('mysql', $mysqlConfig);
+
     }
 
     public static function mainServerCreate(EventRegister $register)
     {
+
         $maxDepth = 3;
         $produce = new Produce('produce', ['maxDepth' => $maxDepth, 'maxCoroutineNum' => 1], false, \EasySwoole\Component\Process\Config::PIPE_TYPE_SOCK_DGRAM, true);
         ServerManager::getInstance()->getSwooleServer()->addProcess($produce->getProcess());
+        ServerManager::getInstance()->getSwooleServer()->addProcess($produce->getProcess());
 
         $consume = new Consume('consume', ['maxDepth' => $maxDepth, 'maxCoroutineNum' => 1], false, \EasySwoole\Component\Process\Config::PIPE_TYPE_SOCK_DGRAM, true);
+        ServerManager::getInstance()->getSwooleServer()->addProcess($consume->getProcess());
+        ServerManager::getInstance()->getSwooleServer()->addProcess($consume->getProcess());
+        ServerManager::getInstance()->getSwooleServer()->addProcess($consume->getProcess());
+        ServerManager::getInstance()->getSwooleServer()->addProcess($consume->getProcess());
         ServerManager::getInstance()->getSwooleServer()->addProcess($consume->getProcess());
         // TODO: Implement mainServerCreate() method.
     }
