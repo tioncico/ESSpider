c7709a200be9b3a98b7ddc635202483044b1ce47
tioncico <1067197739@qq.com>
2019-07-18 22:56:30 +0800

    2.x版本

---
+++ dev.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2019-01-01
+ * Time: 20:06
+ */
+
+return [
+    'SERVER_NAME' => "EasySwoole",
+    'MAIN_SERVER' => [
+        'LISTEN_ADDRESS' => '0.0.0.0',
+        'PORT'           => 9501,
+        'SERVER_TYPE'    => EASYSWOOLE_WEB_SERVER, //可选为 EASYSWOOLE_SERVER  EASYSWOOLE_WEB_SERVER EASYSWOOLE_WEB_SOCKET_SERVER,EASYSWOOLE_REDIS_SERVER
+        'SOCK_TYPE'      => SWOOLE_TCP,
+        'RUN_MODEL'      => SWOOLE_PROCESS,
+        'SETTING'        => [
+            'worker_num'            => 8,
+            'task_worker_num'       => 8,
+            'reload_async'          => true,
+            'task_enable_coroutine' => true,
+            'max_wait_time'         => 3
+        ],
+    ],
+    'TEMP_DIR'    => null,
+    'LOG_DIR'     => null,
+    /*################ REDIS CONFIG ##################*/
+    'REDIS'       => [
+        'host' => '127.0.0.1',
+        'port' => '6379',
+        'auth' => '',
+        'serialize'=>true
+    ],
+    'MYSQL' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'user' => 'root',
+        'password' => '123456',
+        'database' => 'spider',
+        'timeout' => 5,
+        'charset' => 'utf8mb4',
+        'POOL_MAX_NUM' => 10,
+        'POOL_TIME_OUT' => 0.1
+    ],
+];
